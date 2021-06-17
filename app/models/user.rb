0d75fc9783e2db235b5d1f235f8833eecc498db5
kota883 <78450427+kota883@users.noreply.github.com>
2021-06-17 22:24:06 +0900

    age, genderモデル作成（ActiveHash）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender
+  belongs_to :age
 end
