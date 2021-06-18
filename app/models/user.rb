7cced4532d0d6c7d03921acc9c91a1e190643565
kota883 <78450427+kota883@users.noreply.github.com>
2021-06-18 16:56:00 +0900

    user-reviewのアソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :reviews
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :gender
   belongs_to :age
