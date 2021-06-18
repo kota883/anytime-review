399fb11dd0bf82d8a77893c827c8460d958aa7d1
kota883 <78450427+kota883@users.noreply.github.com>
2021-06-18 09:49:11 +0900

    Rubocop実行

--- app/models/gender.rb
+++ app/models/gender.rb
@@ -1,12 +1,11 @@
 class Gender < ActiveHash::Base
-	self.data = [
-		{ id: 1, name: '--' },
-		{ id: 2, name: '男性' },
-		{ id: 3, name: '女性' },
-		{ id: 4, name: 'その他' }
-	]
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '男性' },
+    { id: 3, name: '女性' },
+    { id: 4, name: 'その他' }
+  ]
 
-	include ActiveHash::Associations
+  include ActiveHash::Associations
   has_many :users
-	
-	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     validates :gender_id, :age_id
   end
 
-    # パスワードは半角英数字混合での入力が必須
-    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  # パスワードは半角英数字混合での入力が必須
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: VALID_PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
