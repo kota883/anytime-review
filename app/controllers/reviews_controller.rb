55646af09f479a5679c60969ac9e7af8fcf84b0b
kota883 <78450427+kota883@users.noreply.github.com>
2021-06-19 15:54:49 +0900

    投稿情報編集ページ実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_review, only: [:show, :edit, :update]
+  before_action :user_id_verification, only: [:edit, :update]
 
   def index
     @reviews = Review.order('created_at DESC')
@@ -19,7 +22,17 @@ class ReviewsController < ApplicationController
   end
 
   def show
-    @review = Review.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to review_path(@review)
+    else
+      render :edit
+    end
   end
 
   private
@@ -27,5 +40,13 @@ class ReviewsController < ApplicationController
   def review_params
     params.require(:review).permit(:store_name, :facility_id, :customer_base_id, :access_id, :prefectures_id,
                                    :bicycle_parking_id, :parking_lot_id, :crowded_time_id, :staff_id, :text).merge(user_id: current_user.id)
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def user_id_verification
+    redirect_to root_path unless current_user.id == @review.user_id
   end
 end
